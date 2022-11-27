e4f134e19ae3f854510d179b33ea5be5de8b1336
Mukul Kushwaha <110972197+Mukul-only@users.noreply.github.com>
2022-11-27 21:58:41 +0530

    Add files via upload

---
+++ SignUp/signup.js
@@ -0,0 +1,24 @@
+
+function verifyPassword(){
+    var ps=document.getElementById("password").value;
+    var cps=document.querySelector("#con-password").value;
+    if(ps.length < 8)
+    {
+        document.getElementById("messege").classList.add("messege");
+       document.getElementById("messege").innerHTML="** Password length must be grater than 8 characters!";
+        return false;
+    }
+    if(ps.length > 15)
+    {
+        document.getElementById("messege").classList.add("messege");
+       document.getElementById("messege").innerHTML="** Password length must not exceed 15 characters!";
+
+        return false;
+    }
+    if(ps != cps)
+    {
+        document.querySelector("#con-messege").classList.add("messege");
+        document.querySelector("#con-messege").innerHTML="** Password not matched!";
+        return false;
+    }
+}
